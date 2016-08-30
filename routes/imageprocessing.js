3bded927e7fd846640a8144f8ab24bb7512c44f4
Jeonghwan Im <codingssam@gmail.com>
2016-08-31 08:33:38 +0900

    formidable 사용 시 parse() 함수의 fields 인자에서 배열 인식이 안되는 문제를 field 이벤트 처리를 이용해 커스터마이징

--- routes/imageprocessing.js
+++ routes/imageprocessing.js
@@ -12,7 +12,33 @@ router.get('/', function(req, res, next) {
 router.post('/', function(req, res, next) {
   var uploadDir = '/uploads/images';
   var imageUrl = "http://localhost:3000/images/";
+  var formFields = {};
   var form = new formidable.IncomingForm();
+  form.on('field', function(name, value) {
+    function makeFormFields(prop, val) {
+      if (!formFields[prop]) {
+        formFields[prop] = val;
+      } else {
+        if (formFields[prop] instanceof Array) { // 배열일 경우
+          formFields[prop].push(val);
+        } else { // 배열이 아닐 경우
+          var tmp = formFields[prop];
+          formFields[prop] = [];
+          formFields[prop].push(tmp);
+          formFields[prop].push(val);
+        }
+      }//
+    }
+    var re1 = /\[\]/;
+    var re2 = /\[\d+\]/;
+    if (name.match(re1)) {
+      name = name.replace(re1, '');
+    } else if (name.match(/\[\d+\]/)) {
+      name = name.replace(re2, '');
+    }
+    makeFormFields(name, value);
+  });
+
   form.uploadDir = path.join(__dirname, '..', uploadDir);
   form.keepExtensions = true;
   form.multiples = false;
@@ -20,8 +46,9 @@ router.post('/', function(req, res, next) {
     if (err) {
       return next(err);
     }
-    var width = fields["width"];
-    var height = fields["height"];
+    var width = formFields["width"];
+    var height = formFields["height"];
+    var msg = formFields["msg"];
     var srcImagePath = files["picts"].path;
     var destThumbnailPath =
       path.join(path.dirname(srcImagePath), path.basename(srcImagePath, path.extname(srcImagePath))) +
@@ -33,7 +60,8 @@ router.post('/', function(req, res, next) {
       res.send({
         message: "a thumbnail image created!!!",
         original_url: imageUrl + path.basename(srcImagePath),
-        thumb_url: imageUrl + path.basename(destThumbnailPath)
+        thumb_url: imageUrl + path.basename(destThumbnailPath),
+        msg: msg
       })
     })
   });
